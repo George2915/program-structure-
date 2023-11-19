0e8042e8670a14931f69a4fe2cd759ccacd21f69
George2915 <shtoparelka@gmail.com>
2023-11-19 12:38:59 +0200

    basic program structure and it's meaning

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+// # - is a preprocessor directive
+// < > - look in the directory where the standart libraries are kept
+// include - include the following library 
+// iostream - the library that is to be included
+using namespace std;
+//use namespace std for a shortcut 
+//std::cout << "Hello World!";
+int main()
+/*
+* int main() ... - defines a new function, 
+  by standart function "main" will be called upon execution of the program 
+* int is what called the function's return type. 0 - success, if no "return" then it's 
+*/ 
+{
+    //int short1 = sizeof("short");
+    //int short2 = sizeof(short);
+    //// sizeof( variable type ) ie : sizeof(int) - print the size of different variable types.
+    //cout << short1 << short2 << endl<<short1;
+    //int numberToShow = 123;
+    //cout <<"The value of number is "<<numberToShow<<endl;
+    //cout << "int size = " << sizeof(numberToShow) << endl;
+
+    
+
+    return 0;
+}
